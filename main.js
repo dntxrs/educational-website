2a3fc19daafb6e80e70f5eeb67f8b8c63c1282eb
dntxrs <unhappyfoxe@gmail.com>
2023-05-05 14:53:02 +0700

    making personal educational website

---
+++ main.js
@@ -0,0 +1,48 @@
+// mengubah style navbar ketika di scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+} )
+
+// Show/hide faq 
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // Mengubah icon 
+        const icon = faq.querySelector('.faq-icon i');
+        if (icon.className == "uil uil-plus") {
+            icon.className = "uil uil-minus";
+        }
+        else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
+
+    // SHOW/HIDE NAV MENU 
+
+    const menu = document.querySelector('.nav-menu');
+    const menuBtn = document.querySelector('#open-menu');
+    const closeBtn = document.querySelector('#close-menu');
+
+    menuBtn.addEventListener('click', () => {
+        menu.style.display ="flex";
+        closeBtn.style.display='inline-block';
+        menuBtn.style.display='none';
+    }
+    )
+
+    // CLOSE NAV MENU 
+
+    const closeNav = () => {
+        menu.style.display ="none";
+        closeBtn.style.display='none';
+        menuBtn.style.display='inline-block';
+        
+    }
+
+    closeBtn.addEventListener('click', closeNav);
